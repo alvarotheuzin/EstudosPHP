ea2a13cd2ec4fdf5ce5a5ca219b45d4f7415d234
alvarotheuzin <alvaro.s@iesb.edu.br>
2024-12-19 21:19:21 -0300

    estudos

---
+++ ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hello World!</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "olá, Mundo! \u{1F30E}";
+        ?>
+    </h1>
+    <p>Estudos</p>
+    
+</body>
+</html>
